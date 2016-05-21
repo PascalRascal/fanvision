48ffb72ad1018629019f6a2daa3bca7066e555c2
pascalrascal <blaisemarchetti@outlook.com>
2016-05-21 12:38:28 -0400

    Added callibration tools

---
+++ Assets/calibrationScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class calibrationScript : MonoBehaviour {
+    public GameObject redBall;
+    public GameObject blueBall;
+    public GameObject greenBall;
+    public GameObject cam;
+
+    //Actually north
+    Vector3 north = new Vector3(-1, 0, 0);
+
+    //Actually east
+    Vector3 east = new Vector3(0, 0, 1);
+
+    Vector3 up = new Vector3(0, 1, 0);
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(redBall, cam.transform.position + north, cam.transform.rotation);
+        Instantiate(blueBall, cam.transform.position + up, cam.transform.rotation);
+        Instantiate(greenBall, cam.transform.position + east, cam.transform.rotation);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
